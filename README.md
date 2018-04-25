080082a2fd8a7ec763c2047358e62de8cafbee5e
Lutz Eichler <github@frickler24.de>
2018-04-25 21:16:29 +0200

    first commit from cloning ziai12 (branch openTec)

---
+++ README.md
@@ -0,0 +1,5 @@
+# ziai12
+Vorbereitendes Material für das Raspi Docker-Camp
+
+Hier stehen verschiedene Scripte, php-Files, Config-Files etc. für Container-Experimente mit dem Raspi.
+
---
+++ arm/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (int ac, char**av) {
+	printf ("Hallo Welt!\n");
+	printf ("Herzlichen Glückwunsch: Deine Docker Infrastruktur läuft auf der arm Architektur!\n");
+	return 0;
+}
