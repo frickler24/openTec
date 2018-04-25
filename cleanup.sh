c6070f456902d44f868bc0ed69ffbd551eaaf2d7
Lutz Eichler <github@frickler24.de>
2018-04-25 22:14:30 +0200

    cleanup erweitert um Löschen des nginx

--- cleanup.sh
+++ cleanup.sh
@@ -5,6 +5,14 @@ docker rm -f $(docker ps -aq)
 sudo apt purge -y docker-ce
 
 rm -rf ziai12
-time git clone -b openTec --single-branch https://github.com/frickler24/ziai12
+time git clone https://github.com/frickler24/openTec ziai12
 
+############################################################
 
+noch den vorhandenen nginx wegwerfen!!!
+
+sudo apt purge -y nginx
+
+for host in DeepSpace9 McCoy NX01Enterprise Scotty Sulu Uhura USSDefiant Voyager Spock ; do
+    echo ssh ${host}  sudo apt purge -y nginx
+done
